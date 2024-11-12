a480eabff0e3296682ca14eeaf50fd0323be9336
critt <cbarsim@gmail.com>
2024-11-12 12:30:31 -0800

    Added support for subject and object namespaces

--- backend/src/demo_web_app.py
+++ backend/src/demo_web_app.py
@@ -24,21 +24,53 @@ app.add_routes(routes)
 sio = socketio.AsyncServer(cors_allowed_origins=[])  # * is bad
 sio.attach(app)
 
+# Define the SubjectNamespace
+@sio.on('connect', namespace='/subject')
+async def connect_subject(sid, environ):
+    print(f'Client connected to /subject: {sid}')
+
+@sio.on('disconnect', namespace='/subject')
+async def disconnect_subject(sid):
+    print(f'Client disconnected from /subject: {sid}')
+
 @asyncio.coroutine
-@sio.on('startGoogleCloudStream')
-async def start_google_stream(sid, config):
-    print(f'Starting streaming audio data from client {sid}')
-    await GoogleSpeechWrapper.start_recognition_stream(sio, sid, config)
+@sio.on('startGoogleCloudStream', namespace='/subject')
+async def start_google_stream_subject(sid, config):
+    print(f'Starting streaming audio data from client {sid} on /subject')
+    await GoogleSpeechWrapper.start_recognition_stream(sio, sid, config, '/subject')
 
-
-@sio.on('binaryAudioData')
-async def receive_binary_audio_data(sid, message):
+@sio.on('binaryAudioData', namespace='/subject')
+async def receive_binary_audio_data_subject(sid, message):
     GoogleSpeechWrapper.receive_data(sid, message)
 
-
-@sio.on('endGoogleCloudStream')
-async def close_google_stream(sid):
-    print(f'Closing streaming data from client {sid}')
+@sio.on('endGoogleCloudStream', namespace='/subject')
+async def close_google_stream_subject(sid):
+    print(f'Closing streaming data from client {sid} on /subject')
     await GoogleSpeechWrapper.stop_recognition_stream(sid)
+
+# Define the ObjectNamespace
+@sio.on('connect', namespace='/object')
+async def connect_object(sid, environ):
+    print(f'Client connected to /object: {sid}')
+
+@sio.on('disconnect', namespace='/object')
+async def disconnect_object(sid):
+    print(f'Client disconnected from /object: {sid}')
+
+@asyncio.coroutine
+@sio.on('startGoogleCloudStream', namespace='/object')
+async def start_google_stream_object(sid, config):
+    print(f'Starting streaming audio data from client {sid} on /object')
+    await GoogleSpeechWrapper.start_recognition_stream(sio, sid, config, '/object')
+
+@sio.on('binaryAudioData', namespace='/object')
+async def receive_binary_audio_data_object(sid, message):
+    GoogleSpeechWrapper.receive_data(sid, message)
+
+@sio.on('endGoogleCloudStream', namespace='/object')
+async def close_google_stream_object(sid):
+    print(f'Closing streaming data from client {sid} on /object')
+    await GoogleSpeechWrapper.stop_recognition_stream(sid)
+
 
 web.run_app(app, port=BACKEND_PORT)
